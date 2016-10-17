c76ee14866c0b547835aac0940a10a8fee59da6d
coleliang <coleliang@qq.com>
2016-10-17 21:26:16 +0800

    20161013

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-import os
-import logging
-import inspect
-
 from crawler.future_code import FutureCode
 from crawler.future_trade import FutureTrade
 from crawler.stock_code import StockCode
 from crawler.stock_trade import StockTrade
-from crawler.stock_sheet import StockSheet
 
 class Crawler:
 
@@ -19,8 +13,7 @@ class Crawler:
     def execute(self):
         self.aq.log("Start")
         FutureCode(self.aq).execute()
-        #FutureTrade(self.aq).execute()
+        FutureTrade(self.aq).execute()
         StockCode(self.aq).execute()
         #StockTrade(self.aq).execute()
-        #StockSheet(self.aq).execute()
         self.aq.log("Stop")
