94a0878a02587df4f3cf3d1d4e3175b83592003f
coleliang <coleliang@qq.com>
2016-11-11 22:55:47 +0800

    20161111

--- modeler/stats_test.r
+++ modeler/stats_test.r
@@ -13,14 +13,18 @@ codes <- dbGetQuery(connect, paste0("SELECT code FROM future_code"))
 
 print("Augmented Dickey¨CFuller Test")
 for (code in codes$code) {
-  data <- dbGetQuery(connect, paste0("SELECT datetime, open, high, low, close, volume, oi FROM future_trade WHERE code='", code, "' AND type=1"))
+  data <- dbGetQuery(connect, paste0("SELECT datetime, open, high, low, close, volume, oi FROM future_trade WHERE code='", code, "' AND type='5'"))
   data$datetime <- as.POSIXlt(data$datetime)
   data$rtn <- c(0, diff(data$c))/data$c
   data <- xts(data[,-1], order.by=data[,1])
-  test <- adf.test(data$close)
-  if (test$p.value < 0.02) {
-    print(paste0(code, ": ", test$p.value))
-  }
+  tryCatch(
+    {
+      adf_test <- adf.test(data$close)
+      if (adf_test$p.value < 0.02) {
+        print(paste0(code, ": ", adf_test$p.value))
+      }
+    },
+    error = function(e) { print(paste("Error ", code)) })
 }
 
 ################################################################################
