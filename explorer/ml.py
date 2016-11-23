d98ede6655b16b7da7b4716c86cfa1c03c833fda
coleliang <coleliang@gmail.com>
2016-11-23 10:08:47 +0800

    20161123

--- explorer/ml.py
+++ explorer/ml.py
@@ -5,46 +5,14 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
 
 class ML:
 
     def __init__(self, aq):
         self.aq = aq
-
-    def execute(self):
-        self.aq.log("Start")
-
-        mysql_connector = connector.connect(host=self.aq.DB_HOST, database=self.aq.DB_NAME,
-                                            user=self.aq.DB_USR, password=self.aq.DB_PWD)
-        code = pd.read_sql("""SELECT code FROM aq.future_code
-                            WHERE code='P' OR code='Y' OR code='RB' OR code='I' OR code='M' OR code='RM';""",
-                               con=mysql_connector)
-
-        for row in code.itertuples():
-            data = pd.read_sql("""SELECT close, volume FROM future_trade
-                               WHERE code='%s' AND type='%s' """ % (row[1], "5"), con=mysql_connector)
-            data["chg"] = data.close.diff()
-            data.iloc[0, 2] = 0
-
-            rtn_lag = 10
-            vol_lag = 10
-            k_fold = 10
-
-            ml_algo = "Logistic Regression"
-            self.aq.log("Code = %s, ML Algorithm = %s" % (row[1], ml_algo))
-            hit_rate = self.cross_check(ml_algo, k_fold,
-                                        self.get_y(data, rtn_lag, vol_lag), self.get_X(data, rtn_lag, vol_lag))
-            self.aq.log("Average Hit Rate = %g%s" % (hit_rate*100, "%"))
-
-            ml_algo = "Navie Bayes"
-            self.aq.log("Code = %s, ML Algorithm = %s" % (row[1], ml_algo))
-            hit_rate = self.cross_check(ml_algo, k_fold,
-                                        self.get_y(data, rtn_lag, vol_lag), self.get_X(data, rtn_lag, vol_lag))
-            self.aq.log("Average Hit Rate = %g%s" % (hit_rate * 100, "%"))
-
-        mysql_connector.close()
-
-        self.aq.log("Stop")
 
     def get_y(self, data, rtn_lag, vol_lag):
         y = data.chg[max(rtn_lag, vol_lag):]
@@ -62,7 +30,7 @@ class ML:
                 X = X.append(xrow, ignore_index=True)
         return X
 
-    def cross_check(self, algo, k_fold, y, X):
+    def cross_check(self, model, k_fold, X, y):
         hit_rate_sum = 0
         stride = round(len(y) / k_fold)
         for i in range(0, k_fold):
@@ -74,15 +42,73 @@ class ML:
             y_test = y[start:stop]
             X_train = pd.concat([X[0:start - 1], X[stop:]])
             y_train = pd.concat([y[0:start - 1], y[stop:]])
-            if (algo == "Logistic Regression"):
-                model = LogisticRegression()
-            elif(algo == "Navie Bayes"):
-                model = GaussianNB()
-            else:
-                model = None
             model.fit(X_train, y_train)
             hit_rate = np.sum(model.predict(X_test) == y_test) / (stop-start)
             hit_rate_sum += hit_rate
-            self.aq.log("    k_fold=%s, %.2f%s" % (i+1, hit_rate*100 , "%"))
+            self.aq.log("  k_fold=%s, %.2f%s" % (i+1, hit_rate*100 , "%"))
 
         return hit_rate_sum/k_fold
+
+    def ml(self, code, type):
+        mysql_connector = connector.connect(host=self.aq.DB_HOST, database=self.aq.DB_NAME,
+                                            user=self.aq.DB_USR, password=self.aq.DB_PWD)
+
+        data = pd.read_sql("""SELECT close, volume FROM future_trade WHERE code='%s' AND type='%s' """ % (code, type),
+                              con=mysql_connector)
+        data["chg"] = data.close.diff()
+        data.iloc[0, 2] = 0
+
+        rtn_lag = 10
+        vol_lag = 10
+        k_fold = 10
+
+        self.aq.log("##########")
+
+        model = LogisticRegression()
+        self.aq.log("Code = %s" % code)
+        self.aq.log("Logistic Regression")
+        hit_rate = self.cross_check(model, k_fold, self.get_X(data, rtn_lag, vol_lag), self.get_y(data, rtn_lag, vol_lag))
+        self.aq.log("Average Hit Rate = %g%s" % (hit_rate * 100, "%"))
+
+        self.aq.log("##########")
+
+        model = GaussianNB()
+        self.aq.log("Code = %s" % code)
+        self.aq.log("Naive Bayes")
+        hit_rate = self.cross_check(model, k_fold, self.get_X(data, rtn_lag, vol_lag), self.get_y(data, rtn_lag, vol_lag))
+        self.aq.log("Average Hit Rate = %g%s" % (hit_rate * 100, "%"))
+
+        self.aq.log("##########")
+
+        model = KNeighborsClassifier()
+        self.aq.log("Code = %s" % code)
+        self.aq.log("K Neighbors")
+        hit_rate = self.cross_check(model, k_fold, self.get_X(data, rtn_lag, vol_lag), self.get_y(data, rtn_lag, vol_lag))
+        self.aq.log("Average Hit Rate = %g%s" % (hit_rate * 100, "%"))
+
+        self.aq.log("##########")
+
+        model = DecisionTreeClassifier()
+        self.aq.log("Code = %s" % code)
+        self.aq.log("Decision Tree")
+        hit_rate = self.cross_check(model, k_fold, self.get_X(data, rtn_lag, vol_lag), self.get_y(data, rtn_lag, vol_lag))
+        self.aq.log("Average Hit Rate = %g%s" % (hit_rate * 100, "%"))
+
+        self.aq.log("##########")
+
+        model = SVC()
+        self.aq.log("Code = %s" % code)
+        self.aq.log("Support Vector Machine")
+        hit_rate = self.cross_check(model, k_fold, self.get_X(data, rtn_lag, vol_lag), self.get_y(data, rtn_lag, vol_lag))
+        self.aq.log("Average Hit Rate = %g%s" % (hit_rate * 100, "%"))
+
+        self.aq.log("##########")
+
+        mysql_connector.close()
+
+    def execute(self):
+        self.aq.log("Start")
+
+        self.ml("P", "d")
+
+        self.aq.log("Stop")
